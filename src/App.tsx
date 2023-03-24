36ee1b255191f2c52dd710a2ea355dde55eaa7bb
VivianaS14 <vasorianp@gmail.com>
2023-03-24 14:22:38 -0500

    Config styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline ">Hello</h1>
+      <p className="font-light">Bienvenidos a la pokedex!</p>
     </div>
   );
 };
