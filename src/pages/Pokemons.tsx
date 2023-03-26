76e20430334e1e10e6bb4d2f397cfeaa185bcc5f
VivianaS14 <vasorianp@gmail.com>
2023-03-25 21:12:28 -0500

    feat: loading more pokemons

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import { PokeballIcon } from "../assets";
 import { Header, Footer, PokemonItem, LoadingScreen } from "../components";
 import { usePokemons } from "../hooks";
 
 const Pokemons = () => {
   const [query, setQuery] = useState<string>("");
+  const [first, setFirst] = useState<number>(0);
+  const [last, setLast] = useState<number>(20);
   const pokemonsQuery = usePokemons({ query });
+
+  const handleLoadMore = () => {
+    setFirst(first + 10);
+    setLast(last + 10);
+  };
 
   return (
     <>
@@ -15,10 +23,18 @@ const Pokemons = () => {
             <LoadingScreen />
           ) : (
             pokemonsQuery.data?.results
-              .slice(0, 50)
+              .slice(first, last)
               .map((pokemon) => <PokemonItem key={pokemon.id} data={pokemon} />)
           )}
         </nav>
+        <button
+          type="button"
+          onClick={handleLoadMore}
+          className="w-full flex justify-center py-4 bg-gradient-to-r from-red-500 to-slate-300 bg-clip-text text-transparent text-md font-bold cursor-pointer"
+        >
+          <img src={PokeballIcon} alt="More Button" className="w-6 h-6 mr-2" />
+          More Pokemons
+        </button>
       </main>
       <Footer />
     </>
